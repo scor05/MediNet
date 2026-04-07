8a118c7b14557c43027098a94301d93785f8c8b8
LuisPHernandez <lphm0105@gmail.com>
2026-04-07 17:57:57 -0600

    Eliminar endpoint de guardar usuarios

--- backend/laravel/routes/api/users.php
+++ backend/laravel/routes/api/users.php
@@ -5,10 +5,9 @@ use App\Http\Controllers\DoctorSpecialtyController;
 use App\Http\Controllers\UserController;
 
 Route::prefix('users')->group(function () {
-    Route::get('/',        [UserController::class, 'index']);
-    Route::get('/{id}',    [UserController::class, 'show']);
-    Route::post('/',       [UserController::class, 'store']);
-    Route::patch('/{id}',  [UserController::class, 'update']);
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::patch('/{id}', [UserController::class, 'update']);
     Route::delete('/{id}', [UserController::class, 'destroy']);
 
     Route::get('/{doctorId}/specialties', [DoctorSpecialtyController::class, 'index']);
