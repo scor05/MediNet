8f99eaeb29bda74d0bfdcb370661f1454aad7eec
Santiago Cordero <santicordero28@gmail.com>
2026-07-13 18:52:09 -0600

    Ajustes para despliegue en nueva VM

--- frontend/lib/config/app_config.dart
+++ frontend/lib/config/app_config.dart
@@ -2,6 +2,8 @@ class AppConfig {
   static const String supabaseUrl = 'https://alnbzufubvxmikusvynx.supabase.co';
   static const String supabaseAnonKey =
       'sb_publishable_IzRm1fHfe93THS5bPeDUOA_1kuJqs0M';
-  static const String apiUrl = 'http://34.51.81.158:8880/api'; // Servidor
-  // static const String apiUrl = 'http://localhost:8880/api'; // Local
+  static const String apiUrl = String.fromEnvironment(
+    'API_URL',
+    defaultValue: 'https://medinet.lat/api',
+  );
 }
